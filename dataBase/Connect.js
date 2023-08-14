f6db7a87fdbb105c5864f069b2a85bdbbef6ac13
Aman Deep Sharma <amandeepsharma8944@gmail.com>
2023-08-14 17:05:17 +0530

    env changed

--- dataBase/Connect.js
+++ dataBase/Connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const Connect=async ()=>{
     try {
-      await mongoose.connect(`mongodb+srv://${process.env.USERNAME_MONGO}:${process.env.PASSWORD_MONGO}@project1.y98wgwv.mongodb.net/googleclassroom`, { useNewUrlParser: true, useUnifiedTopology: true })
+      await mongoose.connect(`mongodb+srv://amandeepsharma28032004:amandeep@project1.y98wgwv.mongodb.net/googleclassroom`, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log("connection successfull")
     } catch (error) {
         console.log("error while connecting database")
